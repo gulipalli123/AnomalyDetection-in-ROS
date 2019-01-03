c95341c1c134f6e24dd292d3a3c59fe99a3d5396
kiran <gulipalli.kiran@gmail.com>
2019-01-03 18:10:50 +0100

    Modified AnomalyInserter

--- src/anomaly_detector/src/AnamolyInserterNode.cpp
+++ src/anomaly_detector/src/AnamolyInserterNode.cpp
@@ -21,6 +21,9 @@
 #include <time.h>
 
 
+
+#define MAX_SAMPLING_RATE 7
+#define MIN_SAMPLING_RATE 3
 /*
  * A class to create and write data in a csv file.
  */
@@ -34,7 +37,7 @@ class CSVWriter
 public:
 
 	// Function to fetch data from a CSV File
-	void getData();
+	void  getData();
 	int linesCount_;
 
 	CSVWriter(std::string ifilename, std::string ofilename, std::string delm = ",") :
@@ -56,18 +59,20 @@ void CSVWriter::getData()
 	std::ifstream ifs;
 	ifs.open(inputfileName_.c_str(), std::ifstream::in);
 
-	std::string line = "";
-
-	// Iterate through each line and split the content using delimeter
-	while (getline(ifs, line))
+	if (ifs.is_open()) 
 	{
-		std::vector<std::string> vec;
-		boost::algorithm::split(vec, line, boost::is_any_of(delimeter_));
-		dataList_.push_back(vec);
-		linesCount_++;
+		std::string line = "";
+		// Iterate through each line and split the content using delimeter
+		while (getline(ifs, line))
+		{
+			std::vector<std::string> vec;
+			boost::algorithm::split(vec, line, boost::is_any_of(delimeter_));
+			dataList_.push_back(vec);
+			linesCount_++;
+		}
+		// Close the File
+		ifs.close();
 	}
-	// Close the File
-	ifs.close();
 }
  
 /*
@@ -78,47 +83,59 @@ template<typename T>
 void CSVWriter::addDatainRow(T data, int i)
 {
 	std::fstream filein, fileout;
- 	int j = 0;
-
-	// Open the input file
-	filein.open(inputfileName_, std::ios::in);
 	
 	//Open a new file for updating
 	fileout.open(outputfilename_, std::ios::out);
-
-	std::vector<std::vector<std::string>>::iterator outeritr;
-	std::vector<std::string>::iterator inneritr;
 	
-	outeritr = dataList_.begin();
-	inneritr = (*outeritr).begin();
-
-	//Insert data after the given index
-	dataList_.insert(outeritr + i, data);
-
-	for (; outeritr != dataList_.end(); )
-	{ 
-		inneritr = (*outeritr).begin();
-		for(; inneritr != (*outeritr).end();)
+	if (fileout.is_open())
+	{
+		std::vector<std::vector<std::string>>::iterator outeritr;
+		std::vector<std::string>::iterator inneritr;
+	
+		if(dataList_.size() > 0)
 		{
-			fileout << *inneritr;
-			if (++inneritr != (*outeritr).end())
-				fileout << delimeter_;
+			outeritr = dataList_.begin();
+			inneritr = (*outeritr).begin();
+
+			//Insert data after the given index
+			dataList_.insert(outeritr + i, data);
+
+			for (; outeritr != dataList_.end(); )
+			{ 
+				inneritr = (*outeritr).begin();
+				for(; inneritr != (*outeritr).end();)
+				{
+					fileout << *inneritr;
+					if (++inneritr != (*outeritr).end())
+						fileout << delimeter_;
+				}
+				fileout << "\n";
+				outeritr++;
+			}
 		}
-		fileout << "\n";
-		outeritr++;
+	
+	
+	 
+		// Close the file
+		fileout.close();
 	}
-	
-	
- 
-	// Close the files
-	filein.close();
-	fileout.close();
 }
 
 
 int main(int argc, char **argv)
 {
-     int index = 0, maxSize = 5;
+    int insertSamples = 0 ;
+    
+    if (argc != 2) 
+    {
+    	insertSamples = MIN_SAMPLING_RATE;
+    } 
+    else 
+    {
+       insertSamples = atoi(argv[1]);
+    }
+
+     int index = 0;
      
      /* initialize random seed: */
      srand (time(NULL));
@@ -138,19 +155,33 @@ int main(int argc, char **argv)
 						{ "0xea","28", "17.8", "0","1","0","0","1546436835" },
      						{ "0xea","12", "17.8", "0","0","0","1","1546436836" },
      						{ "0xea","26", "17.8", "1","0","0","0","1546436837" },
-     						{ "0xea","25.4", "11.2", "0","0","1","0","1546436838"}
+     						{ "0xea","25.4", "11.2", "0","0","1","0","1546436838"},
+						{ "0xea","26", "18.4", "1","0","1","0","1546436839" },
+     						{ "0xea","25.4", "12.1", "0","1","1","0","1546436840"}
+						
 					   };
       
      
      
-     if(0 < writer.linesCount_)	index = rand() % writer.linesCount_ + 1;
-     std::cout << "inserted at index = " << index << std::endl;
-     
-     for(int i = 0; i < maxSize; i++)
+     if(0 < writer.linesCount_)
      {
-	// Adding vector to CSV File
-	  writer.addDatainRow(dataList[i], index++);
+	index = rand() % writer.linesCount_ + 1;    
+     	//std::cout << "inserted at index = " << index << std::endl;
      }
+     else
+	std::cout << "Error:: not able to open file or file does not have any data" << std::endl;
+     
+     if( insertSamples > 0  && insertSamples <= MAX_SAMPLING_RATE) 
+     { 
+	     std::cout << "inserting "  << insertSamples << " samples at index "  << index  << std::endl; 
+	     for(int i = 0; i < insertSamples; i++)
+	     {
+		  // Adding vector to CSV File
+		  writer.addDatainRow(dataList[i], index++);
+	     }
+    }
+    else
+	std::cout << "error:samples to be injected can not be greater than 7" << std::endl;
  
   return 0;
 }
--- src/anomaly_detector/src/CcsvReader.cpp
+++ src/anomaly_detector/src/CcsvReader.cpp
@@ -6,6 +6,7 @@
  */
 
 #include "anomaly_detector/CcsvReader.h"
+#include "iostream"
 
 CcsvReader::CcsvReader(std::string filename, std::string delm )
 						:fileName_(filename), delimeter_(delm){
@@ -19,21 +20,28 @@ std::vector<vectorOfStrings> CcsvReader::getData()
 
 	std::vector<vectorOfStrings > dataList;
 
-	std::string line = "";
-	// Ignore first line as it does not have any data
-	ifs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-	// Iterate through each line and split the content using delimeter
-	while (getline(ifs, line))
+	if (ifs.is_open()) 
 	{
-		vectorOfStrings vec;
-		boost::algorithm::split(vec, line, boost::is_any_of(delimeter_));
-		dataList.push_back(vec);
-	}
-	// Close the File
-	ifs.close();
 
-	return dataList;
+		std::string line = "";
+		// Ignore first line as it does not have any data
+		ifs.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+		// Iterate through each line and split the content using delimeter
+		while (getline(ifs, line))
+		{
+			std::vector<std::string> vec;
+			boost::algorithm::split(vec, line, boost::is_any_of(delimeter_));
+			dataList.push_back(vec);
+		}
+		// Close the File
+		ifs.close();
+		
+       }
+       else
+         std::cout << "Not able to open the input file" << std::endl;
+
+      return dataList;
 }
 CcsvReader::~CcsvReader() {
 	// TODO Auto-generated destructor stub
